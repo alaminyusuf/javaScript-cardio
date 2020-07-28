6d7ae28e673037f13806f7a7c109097fc49ea203
Al-amin Yusuf <alaminyusuf131@gmail.com>
2020-07-28 22:11:57 +0100

    algorithm to reverse negative numbers

--- revInt.js
+++ revInt.js
@@ -1,10 +1,19 @@
 function reverseInt(int) {
+  // const revString = int
+  //   .toString()
+  //   .split('')
+  //   .reverse()
+  //   .join('')
+  // return parseInt(revString)
+
+  /////////////////////////
+  // WITH NUMBERS WITH NEGATIVE VALUES
   const revString = int
     .toString()
     .split('')
     .reverse()
     .join('')
-  return parseInt(revString)
+  return parseInt(revString) * Math.sign(int)
 }
 
 const output = reverseInt(1234)
