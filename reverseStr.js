344dce4073490b17e474273fc21bb9fa7b01e9e4
Al-amin Yusuf <alaminyusuf131@gmail.com>
2020-07-28 21:56:27 +0100

    reversed string using for loop with decremention

--- reverseStr.js
+++ reverseStr.js
@@ -1,6 +1,14 @@
 function reverseString(str) {
   // USING JAVASCRIPT METHODS
-  return str.split('').reverse().join('')
+  // return str.split('').reverse().join('')
+
+  // //////////////
+  // USING FOR LOOP WITH DECREMENTION
+  let revString = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    revString = revString + str[i]
+  }
+  return revString
 }
 const output = reverseString('world')
 
