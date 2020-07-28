e198c53a7594edfb6d0ca98a6d8ab3559045752e
Al-amin Yusuf <alaminyusuf131@gmail.com>
2020-07-28 22:06:16 +0100

    capitalize string using map function

--- capitalizeStr.js
+++ capitalizeStr.js
@@ -1,12 +1,20 @@
 function capitalizeString(str) {
   // USING JAVASCRIPT METHOD
-  const strArr = str.toLowerCase().split(' ')
-  for (let i = 0; i < strArr.length; i++) {
-    strArr[i] =
-      strArr[i].substring(0, 1).toUpperCase() +
-      strArr[i].substring(1)
-  }
-  return strArr.join(' ')
+  // const strArr = str.toLowerCase().split(' ')
+  // for (let i = 0; i < strArr.length; i++) {
+  //   strArr[i] =
+  //     strArr[i].substring(0, 1).toUpperCase() +
+  //     strArr[i].substring(1)
+  // }
+  // return strArr.join(' ')
+
+  // /////////////
+  // USINS MAP FUNCTION
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.substr(1))
+    .join(' ')
 }
 
 const output = capitalizeString('i love javascript')
